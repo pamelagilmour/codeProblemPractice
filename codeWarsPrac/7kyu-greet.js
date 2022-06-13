8320820b52e37decca4d1a92ce4c79c1117b6ce4
Pamela Gilmour <65507722+pamelagilmour@users.noreply.github.com>
2022-06-13 10:54:32 -0700

    Add 3 kyus from today, add new dataStructures dir

---
+++ codeWarsPrac/7kyu-greet.js
@@ -0,0 +1,17 @@
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+
+
+function greet (name, owner) {
+	// Add code here
+	// Use conditionals to return the proper message:
+	// case 	return
+	// name equals owner 	'Hello boss'
+	// otherwise 	'Hello guest'
+
+    if (name === owner ) {
+        return 'Hello boss';
+    } else {
+        return 'Hello guest';
+    }
+}
