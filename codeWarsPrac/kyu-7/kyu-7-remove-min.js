b480d68893db0df3e9a4f8734b534282cde252d3
Pamela Gilmour <65507722+pamelagilmour@users.noreply.github.com>
2022-07-02 10:46:41 -0700

    Add more kyu

--- codeWarsPrac/kyu-7/kyu-7-remove-min.js
+++ codeWarsPrac/kyu-7/kyu-7-remove-min.js
@@ -1,9 +1,3 @@
-// The museum of incredible dull things
-
-// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
-
-// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
-
 // Task
 
 // Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
@@ -12,35 +6,64 @@
 // Examples
 
 // * Input: [1,2,3,4,5], output= [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
 
 function removeSmallest(numbers) {
-//   throw "TODO: removeSmallest";
-    console.log('hello bitch')
-    // let firstnum;
-    // let secondnum;
-    // let min;
-    // for (let i = 0; i < numbers.length; i++) {
-        // console.log(numbers[i])
-        // if (numbers[i] < numbers[i+1]) {
-        //     console.log(min);
-            // console.log(numbers[i], numbers[i+1]);
-            // console.log(numbers[i+1])
-            // min = numbers[i]
-        // }
-    // }
-    // console.log(min);
-    // let newArr = [];
 
-    // numbers.forEach((element) => {
-    //     if(numbers[element] !== min) {
-    //         newArr.push(numbers[element])
-    //     }
-    // })
+    if (numbers === null) {
+        return numbers
+        // return Array;
+    }
 
+
+    let min = numbers[0];
+
+    for( let i = 0; i < numbers.length; i++) {
+        
+        // secondNum = numbers[i+1];
+        if(numbers[i] < min) {
+            min = numbers[i]
+        }
+
+    }
+    
+    let newArr = [];
+    let mincount = 0;
+
+    for( let i = 0; i < numbers.length; i++) {
+        
+        if (numbers[i] === min) {
+            mincount++;
+        }
+        console.log("mincount is: ", mincount);
+
+        if (mincount === 1) {
+            if (numbers[i] !== min) {
+            
+                newArr.push(numbers[i])
+            }
+        // } else if(mincount > 1) {
+            // if (mincount > 1 && numbers[i] !== min) {
+                // newArr.push(numbers[i])
+            // } else if(mincount > 1 && numbers[i] === min) {/
+                // newArr.push(numbers[i])
+
+            // }
+
+        }
+        
+
+    }
+
+    
     // return newArr
+
 }
+
+// removeSmallest([1, 2, 3, 4, 5])
 
 console.log(removeSmallest([1, 2, 3, 4, 5]));
 console.log(removeSmallest([5, 3, 2, 1, 4]));
 console.log(removeSmallest([2, 2, 1, 2, 1]));
-// console.log(removeSmallest([]));
+console.log(removeSmallest([]));
