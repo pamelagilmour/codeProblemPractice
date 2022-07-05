66292d29984ec9642597bf3633dcd447274f33fb
Pamela Gilmour <65507722+pamelagilmour@users.noreply.github.com>
2022-07-05 14:18:13 -0700

    Add completed kyu

---
+++ codeWarsPrac/kyu-7/kyu-7-like-button.js
@@ -0,0 +1,34 @@
+function likeOrDislike(buttons) {
+    if(buttons.length === 1){
+        return buttons[0]
+    } else if (buttons.length === 0) {
+        return 'Nothing';
+    }
+
+    let buttonState = 'Nothing';
+
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i] === buttonState) {
+            buttonState = 'Nothing'
+
+        } else if (buttons[i] === 'Like') {
+            buttonState = 'Like'
+        } else if (buttons[i] === 'Dislike'){
+            buttonState = 'Dislike'
+        }
+    }
+
+    return buttonState;
+
+}
+
+console.log( likeOrDislike(['Dislike']), " Dislike" );
+console.log( likeOrDislike(['Like', 'Like']), " Nothing" );
+console.log( likeOrDislike(['Dislike', 'Like']), " Like" );
+console.log( likeOrDislike(['Like', 'Dislike', 'Dislike']), " Nothing" );
+console.log( likeOrDislike(['Dislike', 'Dislike']), " Nothing" );
+console.log( likeOrDislike(['Like', 'Like', 'Like']), " Like" );
+console.log( likeOrDislike(['Like', 'Dislike']), " Dislike" );
+console.log( likeOrDislike(['Dislike', 'Like', 'Dislike']), " Dislike" );
+console.log( likeOrDislike(['Like', 'Like', 'Dislike', 'Like', 'Like', 'Like', 'Like', 'Dislike']), " Dislike" );
+console.log( likeOrDislike([]), " Nothing" );
