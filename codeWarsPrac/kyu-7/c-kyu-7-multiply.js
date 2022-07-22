f9cf6e3fcbdf27bc8188e5da5f2b6e33a36cafc9
Pamela Gilmour <pgilmour@kqed.org>
2022-07-22 10:01:07 -0700

    Add kew kata

---
+++ codeWarsPrac/kyu-7/c-kyu-7-multiply.js
@@ -0,0 +1,9 @@
+
+
+// Implement a function which multiplies two numbers.
+function multiply(x, y) {
+    return x * y;
+  }
+
+//   Other Solutions that I liked:
+// let multiply = (a, b) => a * b;
